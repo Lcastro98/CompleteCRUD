3048a9208ceeef70e83beb1cb0e803c4ccd802b4
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-02 23:07:13 -0500

    Controlador
    Se agrega el controlador, quien contiene los metodos http a utilizar y sus rutas

--- crud_be/src/main/java/co/com/sofka/crud_be/TodoController.java
+++ crud_be/src/main/java/co/com/sofka/crud_be/TodoController.java
@@ -1,2 +1,40 @@
-package co.com.sofka.crud_be;public class TodoController {
+package co.com.sofka.crud_be;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> List(){
+        return service.List();
+    }
+
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId() != null) {
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualizar");
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id){
+        service.delete(id);
+    }
+
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
 }
