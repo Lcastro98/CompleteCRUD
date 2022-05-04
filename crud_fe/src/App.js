edd962c27df834e386526fecc8de288a7cd4facd
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-03 22:41:59 -0500

    Funcionalidades
    Se agrega la funcionalidad de editar y eliminar

--- crud_fe/src/App.js
+++ crud_fe/src/App.js
@@ -3,15 +3,16 @@ import { createContext, useContext, useRef, useEffect, useReducer, useState } fr
 const HOST_API = "http://localhost:8080/api";
 
 const initialState = {
-  list: []
+  list: [],
+  item: {}
 };
 
 const Store = createContext(initialState)
 
 const Form = () => {
   const formRef = useRef(null);
-  const { dispatch } = useContext(Store);
-  const [state, setState] = useState({});
+  const { dispatch, state: {item} } = useContext(Store);
+  const [state, setState] = useState(item);
 
   const onAdd = (event) => {
     event.preventDefault();
@@ -37,14 +38,41 @@ const Form = () => {
     });
   }
 
+  const onEdit = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((todo) => {
+      dispatch({ type: "update-item", item: todo });
+      setState({ name: ""});
+      formRef.current.reset();
+    });
+  }
+
   return <form ref={formRef}>
     <input 
       type="text" 
       name="name" 
+      defaultValue={item.name}
       onChange={(event) => {
       setState({ ...state, name: event.target.value })
     }} ></input>
-    <button onClick={onAdd}>Agregar</button>
+    {item.id && <button onClick={onEdit}>Actualizar</button>} 
+    {!item.id && <button onClick={onAdd}>Agregar</button>}
+    
   </form>
 }
 
@@ -58,6 +86,19 @@ const List = () => {
       dispatch({type: "update-list", list})
     })
   }, [state.list.length, dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE"
+    })
+    .then((list) => {
+      dispatch({ type: "delete-item", id })
+    })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  }
 
   return <div>
     <table>
@@ -73,7 +114,9 @@ const List = () => {
           return <tr key={todo.id}>
             <td>{todo.id}</td>
             <td>{todo.name}</td>
-            <td>{todo.isCompleted}</td>
+            <td>{todo.isCompleted === true ? "SI" : "NO"}</td>
+            <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
+            <td><button onClick={() => onEdit(todo)}>Editar</button></td>
           </tr>
         })}
       </tbody>
@@ -83,8 +126,23 @@ const List = () => {
 
 function reducer(state, action) {
   switch (action.type) {
+    case 'update-item':
+      const listUpdateEdit = state.list.map((item) => {
+        if(item.id === action.item.id){
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {}}
+    case 'delete-item':
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate}
     case 'update-list':
       return {...state, list: action.list}
+    case 'edit-item':
+      return {...state, item: action.item}
     case 'add-item':
       const newList = state.list;
       newList.push(action.item);
